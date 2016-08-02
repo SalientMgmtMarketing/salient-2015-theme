cee9e124ba243564705d62efd2598d0c906907aa
Paul Stonier <pstonier@gmail.com>
2016-08-02 13:32:19 -0400

    Reapplied the updated leadlander code

--- footer-boxy.php
+++ footer-boxy.php
@@ -14,9 +14,16 @@
 <?php wp_footer(); ?>
 
 <!-- LeadLander Code -->
-<script type="text/javascript" language="javascript">llactid=13443</script> 
-<script type="text/javascript" language="javascript" src="http://t2.trackalyzer.com/trackalyze.js"></script>
-<?php if (is_page_template('page-landing-page.php') || is_page('contact')) { ?>
+<script type="text/javascript" language="javascript"> 
+      var sf14gv = 13443; 
+      (function() { 
+      var sf14g = document.createElement('script'); sf14g.type = 'text/javascript'; sf14g.async = true; 
+      sf14g.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 't.sf14g.com/sf14g.js'; 
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sf14g, s); 
+      })(); 
+</script>
+<?php if (is_page_template('page-landing-page.php') || is_page('contact') || is_page('contact-us') ) { ?>
+
   <script type="text/javascript">llfrmid=13443</script> 
   <script type="text/javascript" src="https://formalyzer.com/formalyze_init.js"></script>
   <script type="text/javascript" src="https://formalyzer.com/formalyze_call_secure.js"></script>
