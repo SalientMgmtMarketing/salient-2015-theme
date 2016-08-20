bcfdf611ad372d0d1902d7d3ccb666624fcd9aec
Paul Stonier <pstonier@gmail.com>
2016-08-19 21:53:18 -0400

    Moved youtube embed scripts to the footer

--- functions.php
+++ functions.php
@@ -367,6 +367,15 @@ function is_tree($pid)
   }
 };
 
+
+function remove_head_scripts() {
+  remove_action( 'wp_head', 'youtube_embed_add_to_head' );
+  add_action( 'wp_footer', 'youtube_embed_add_to_head' );
+}
+add_action( 'wp_enqueue_scripts', 'remove_head_scripts' );
+
+
+
 // http://www.gravityhelp.com/forums/topic/input-on-single-line-text-is-cut-off-after
 // this handles converting the entered value to hex for storage
 //
