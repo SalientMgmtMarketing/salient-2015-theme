b9ab2af64f74f53de04d578a4f02aacf161a37e5
Paul Stonier <pstonier@gmail.com>
2016-10-06 14:39:52 -0400

    Updated the 404 template

--- 404.php
+++ 404.php
@@ -10,12 +10,14 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'salient-2015' ); ?></h1>
+			<article class="error-404 not-found">
+				<header class="page-header hero">
+                  <div class="wrap">
+					<h1 class="page-title"><?php _e( 'Sorry! That page can&rsquo;t be found.', 'salient-2015' ); ?></h1>
+                  </div><!--.wrap-->
 				</header><!-- .page-header -->
-
-				<div class="page-content">
+                <div class="wrap">
+				<div class="entry-content">
 					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'salient-2015' ); ?></p>
 
 					<?php get_search_form(); ?>
@@ -47,8 +49,9 @@ get_header(); ?>
 
 					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
 
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
+				</div><!-- .entry-content -->
+              </div><!--.wrap-->
+			</article><!-- .error-404 -->
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
