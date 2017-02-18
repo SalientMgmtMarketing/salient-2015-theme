f9b707293ebfd7feddde240c36dff47840d4017d
Paul Stonier <pstonier@gmail.com>
2017-02-17 19:09:42 -0500

    Updated version number

--- functions.php
+++ functions.php
@@ -226,12 +226,12 @@ add_action( 'widgets_init', 'salient_2015_widgets_init' );
  * Enqueue scripts and styles.
  */
 function salient_2015_scripts() {
-	wp_enqueue_style( 'salient-2015-style', get_stylesheet_uri() ,'' , '1.2.3');
+	wp_enqueue_style( 'salient-2015-style', get_stylesheet_uri() ,'' , '1.2.4');
   	wp_enqueue_style( 'salient-2015-fancybox-style', get_template_directory_uri() . '/js/fancybox/jquery.fancybox.css' );
 	wp_enqueue_script( 'jquery' );
 
 	wp_enqueue_script( 'salient-2015-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-	wp_enqueue_script( 'salient-2015-custom', get_template_directory_uri() . '/assets/js/scripts.js', array('jquery'), '1.2.3', true );
+	wp_enqueue_script( 'salient-2015-custom', get_template_directory_uri() . '/assets/js/scripts.js', array('jquery'), '1.2.4', true );
   	wp_enqueue_script( 'salient-2015-custom-footer', get_template_directory_uri() . '/assets/js/scripts-footer.js', array('jquery'), '', true );
   
 
