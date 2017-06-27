b5c11ed67fb1eaab2ecbe79c11a8a9f213957daf
Paul Stonier <pstonier@gmail.com>
2017-06-27 13:55:16 -0400

    Added citation to blog posts with publication cited and removed name from footer bylines.

--- content-single.php
+++ content-single.php
@@ -5,23 +5,23 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header hero" 
-			<?php if ( get_field('hero_image')) { ?> 
-				style="background-image:url('<?php 
+	<header class="entry-header hero"
+			<?php if ( get_field('hero_image') && get_field('hero_image') !== 'https://www.salient.com/wp-includes/images/media/default.png' ) { ?>
+				style="background-image:url('<?php
 				$thumb_id = get_post_thumbnail_id();
 				$thumb_url = wp_get_attachment_image_src($thumb_id,'full', true);
-				echo $thumb_url[0]; 
+				echo $thumb_url[0];
               ?>');"<?php
 			}
 		?>>
       <div class="wrap">
         <?php if ( function_exists('yoast_breadcrumb') ) {yoast_breadcrumb('<p id="breadcrumbs">','</p>');} ?>
         <div class="category-link">
-          <?php 
+          <?php
           $categories = get_the_category();
           if ( ! empty( $categories ) ) {
             echo '<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
-          } ?> 
+          } ?>
         </div><!--.category-link-->
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
@@ -30,20 +30,26 @@
               <?php salient_2015_posted_on(); ?>
           </div><!-- .entry-meta -->
         <?php } ?>
-        
+        <?php if ( get_field('publication') ) { ?>
+					<div class="entry_meta byline">
+						<p>Published by <?php if ( get_field('publication_url') ) { ?><a href="<?php the_field('publication_url');  ?>" rel="canonical" target="_blank"><?php } ?>
+								<?php the_field('publication'); ?><?php if ( get_field('publication_url') ) { ?></a><?php } ?></p>
+						<p>Publication Date: <?php the_field('publication_date'); ?></p>
+					</div>
+				<?php	} ?>
       </div><!--.wrap-->
 	</header><!-- .entry-header -->
     <div class="wrap">
       <div class="entry-content">
 
-        <?php 
+        <?php
         // Get the featured image, if there is one.
         if ( has_post_thumbnail() && !get_field('hero_image')) { ?>
           <div class="featured-image">
               <?php echo get_the_post_thumbnail( '', 'blog-feed' ); ?>
           </div><!--.featured-image-->
         <?php } ?>
-        
+
 		<div class="text-block"><?php the_content(); ?></div>
 		<?php
 			wp_link_pages( array(
