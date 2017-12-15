bd1a63d196bb948c9602aea4b87fbd18d3fc7f0e
Paul Stonier <pstonier@salient.com>
2017-12-15 11:26:31 -0500

    Adjusted blog styling

--- gulpfile.js
+++ gulpfile.js
@@ -11,16 +11,16 @@ var cssnano = require('gulp-cssnano');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('concatScripts', function () {
-    return gulp.src([
-            '!./assets/js/scripts.js',
-            './js/navigation.js',
-            './node_modules/js-cookie/src/js.cookie.js',
-            './js/flickity.pkgd.min.js',
-            './js/fancybox/**/*.js',
-            './js/**/*.js',
-            './inc/**/*.js',
-            '!./js/customizer.js'
-    ])
+  return gulp.src([
+    '!./assets/js/scripts.js',
+    './js/navigation.js',
+    './node_modules/js-cookie/src/js.cookie.js',
+    './js/flickity.pkgd.min.js',
+    './js/fancybox/**/*.js',
+    './js/**/*.js',
+    './inc/**/*.js',
+    '!./js/customizer.js'
+  ])
     .pipe(maps.init())
     .pipe(concat('scripts.js'))
     .pipe(maps.write('./'))
@@ -28,14 +28,14 @@ gulp.task('concatScripts', function () {
 });
 
 gulp.task('concatScriptsFooter', function () {
-    return gulp.src([
-            'node_modules/gsap/src/minified/TweenMax.min.js',
-            'node_modules/scrollmagic/scrollmagic/minified/ScrollMagic.min.js',
-            'node_modules/scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js',
-            'assets/js/footer.js',
-            'assets/js/homepage-slides.js',
-            'assets/js/homepage-tabs.js'
-    ])
+  return gulp.src([
+    'node_modules/gsap/src/minified/TweenMax.min.js',
+    'node_modules/scrollmagic/scrollmagic/minified/ScrollMagic.min.js',
+    'node_modules/scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js',
+    'assets/js/footer.js',
+    'assets/js/homepage-slides.js',
+    'assets/js/homepage-tabs.js'
+  ])
     .pipe(maps.init())
     .pipe(concat('scripts-footer.js'))
     .pipe(maps.write('./'))
@@ -43,31 +43,31 @@ gulp.task('concatScriptsFooter', function () {
 });
 
 gulp.task('minifyScripts', ['concatScripts'], function () {
-    return gulp.src('./assets/js/scripts.js')
-            .pipe(uglify())
-            .pipe(gulp.dest('./assets/js'));
+  return gulp.src('./assets/js/scripts.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./assets/js'));
 });
 
 gulp.task('browser-sync', function () {
-    browserSync.init({
-        proxy: "salient.dev/", notify: false
-    });
+  browserSync.init({
+    proxy: 'salient.dev/', notify: false
+  });
 });
 
 gulp.task('compileSass', function () {
-    return gulp.src('./sass/**/*.scss')
-            .pipe(maps.init())
-            .pipe(sass({includePaths: require('bourbon').includePaths}))
-            .pipe(autoprefixer())
-            .pipe(cssnano())
-            .pipe(maps.write('./'))
-            .pipe(gulp.dest('./'))
-            .pipe(reload({stream:true}));
+  return gulp.src('./sass/**/*.scss')
+    .pipe(maps.init())
+    .pipe(sass({includePaths: require('bourbon').includePaths}))
+    .pipe(autoprefixer())
+    .pipe(cssnano())
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('./'))
+    .pipe(reload({stream:true}));
 });
 
 gulp.task('watchFiles', function(){
-    gulp.watch(['./sass/**/*.scss','./scss/*.scss'],['compileSass']);
-        gulp.watch(['./js/**.js','./inc/**/*.js','./assets/**.js'], ['concatScripts','concatScriptsFooter']);
+  gulp.watch(['./sass/**/*.scss','./scss/*.scss'],['compileSass']);
+  gulp.watch(['./js/**.js','./inc/**/*.js','./assets/**.js'], ['concatScripts','concatScriptsFooter']);
 });
 
 
@@ -75,7 +75,7 @@ gulp.task('watchFiles', function(){
 
 
 
-gulp.task('build', ['concatScriptsFooter','minifyScripts', 'compileSass']);
+gulp.task('build', ['concatScripts','concatScriptsFooter','minifyScripts', 'compileSass']);
 
 gulp.task('watch', ['watchFiles']);
 
--- templates/flexible-content-testimonials.php
+++ templates/flexible-content-testimonials.php
@@ -1,48 +1,48 @@
 <?php
 
-           //TESTIMONIALS LAYOUT
-            if(  get_row_layout() == 'rotating_testimonials'): ?>
-            <section <?php if( get_sub_field( 'section_id') ) { ?> id="
-              <?php the_sub_field('section_id'); ?>"
-              <?php } ?> class="row testimonials">
-                <div class="wrap above-fixed">
-                  <?php if( get_sub_field('section_title') ): ?>
-                    <h2 class="section-title"><?php the_sub_field('section_title'); ?></h2>
-                  <?php endif; ?>
-                  <?php if ( have_rows('testimonials') ) { ?>
-                  <div class="slider">
-                    <div class="slides">
-                      <?php while ( have_rows('testimonials') ) : the_row(); ?>
-                      <div class="slide">
-                        <?php if( get_sub_field('testimonial_title') ): ?>
-                          <h3 class="testimonial-title"><?php the_sub_field('testimonial_title'); ?></h3>
-                        <?php endif; ?>
-                        <figure class="quote">
-                          <blockquote>
-                            <?php the_sub_field('quote'); ?>
-                          </blockquote>
-                          <figcaption><?php the_sub_field('citation'); ?></figcaption>
-                        </figure>
-                        <?php if ( get_sub_field('link_url') && get_sub_field('logo') ) { ?>
-                          <a href="<?php the_sub_field('link_url'); ?>">
-                        <?php } ?>
-                        <?php $logo = get_sub_field('logo');
-                        if ( $logo ) { ?>
-                          <div class="testimonial-logo">
-                            <img src="<?php echo $logo['url'] ?>" alt="<?php echo $logo['alt']; ?>" />
-                          </div>
-                        <?php } ?>
-                        <?php if ( get_sub_field('link_url') && get_sub_field('logo') ) { ?>
-                          </a>
-                        <?php } ?>
-                      </div>
-                    <?php endwhile; ?>
-                    </div>
-                  </div>
-                  <!-- .slider -->
-                  <?php } ?>
-                </div>
-                <!--.wrap-->
-            </section>
-            <!-- #intro -->
-      <?php endif; ?>
+					 //TESTIMONIALS LAYOUT
+						if(  get_row_layout() == 'rotating_testimonials'): ?>
+						<section <?php if( get_sub_field( 'section_id') ) { ?> id="
+							<?php the_sub_field('section_id'); ?>"
+							<?php } ?> class="row testimonials">
+								<div class="wrap above-fixed">
+									<?php if( get_sub_field('section_title') ): ?>
+										<h2 class="section-title"><?php the_sub_field('section_title'); ?></h2>
+									<?php endif; ?>
+									<?php if ( have_rows('testimonials') ) { ?>
+									<div class="slider">
+										<div class="slides">
+											<?php while ( have_rows('testimonials') ) : the_row(); ?>
+											<div class="slide">
+												<?php if( get_sub_field('testimonial_title') ): ?>
+													<h3 class="testimonial-title"><?php the_sub_field('testimonial_title'); ?></h3>
+												<?php endif; ?>
+												<figure class="quote">
+													<blockquote>
+														<?php the_sub_field('quote'); ?>
+													</blockquote>
+													<figcaption><?php the_sub_field('citation'); ?></figcaption>
+												</figure>
+												<?php if ( get_sub_field('link_url') && get_sub_field('logo') ) { ?>
+													<a href="<?php the_sub_field('link_url'); ?>">
+												<?php } ?>
+												<?php $logo = get_sub_field('logo');
+												if ( $logo ) { ?>
+													<div class="testimonial-logo">
+														<img src="<?php echo $logo['url'] ?>" alt="<?php echo $logo['alt']; ?>" />
+													</div>
+												<?php } ?>
+												<?php if ( get_sub_field('link_url') && get_sub_field('logo') ) { ?>
+													</a>
+												<?php } ?>
+											</div>
+										<?php endwhile; ?>
+										</div>
+									</div>
+									<!-- .slider -->
+									<?php } ?>
+								</div>
+								<!--.wrap-->
+						</section>
+						<!-- #intro -->
+			<?php endif; ?>
