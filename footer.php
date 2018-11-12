37ba35d81da63ae33d9a7b5d44281e68c3d8d0c7
Paul Stonier <pstonier@gmail.com>
2018-11-12 15:43:39 -0500

    Added Close button to Cookies Banner

--- footer.php
+++ footer.php
@@ -16,19 +16,25 @@
 	</div>
 </footer>
 <?php if ( get_field( 'gdpr_cookies_message', 'option' ) ) { ?>
-	<div class="gdpr-cookie-banner gdpr-hide"><?php the_field( 'gdpr_cookies_message', 'option' ); ?></div>
+	<div class="gdpr-cookie-banner gdpr-hide">
+		<?php the_field( 'gdpr_cookies_message', 'option' ); ?>
+		<button class="close-banner">
+			<?php echo '<img src="' . esc_attr( get_template_directory_uri() ) . '/assets/images/close-X-icon.svg" />'; ?>
+			<span class="screen-reader-text">Close Message Window</span>
+		</button>
+	</div>
 <?php } ?>
 
 <?php wp_footer(); ?>
 <script>
 window.addEventListener('load',function(){
-  var setint_thanku = setInterval(function(){
-    if(jQuery('._form-thank-you:contains("Thank You! A link to the case study has been delivered to the email address that was provided.")').is(":visible"))
-    {
-        __gaTracker('send','event','form','submit','download case study');
-        clearInterval(setint_thanku);
-    }
-  },500);
+	var setint_thanku = setInterval(function(){
+		if(jQuery('._form-thank-you:contains("Thank You! A link to the case study has been delivered to the email address that was provided.")').is(":visible"))
+		{
+				__gaTracker('send','event','form','submit','download case study');
+				clearInterval(setint_thanku);
+		}
+	},500);
 })
 </script>
 </body>
