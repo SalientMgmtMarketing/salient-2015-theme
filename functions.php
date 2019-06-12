1c8da821f00bf326201fa72a9d8c04946d1860ed
Paul Stonier <pstonier@salient.com>
2019-06-12 11:22:52 -0400

    Updated styles for header banner

--- functions.php
+++ functions.php
@@ -303,7 +303,7 @@ add_action( 'widgets_init', 'salient_2015_widgets_init' );
  */
 function salient_2015_scripts() {
 
-	$theme_version = '1.6.27';
+	$theme_version = '1.6.28';
 
 	wp_enqueue_style( 'salient-2015-style', get_stylesheet_uri(), '', $theme_version );
 	wp_enqueue_style( 'salient-2015-fancybox-style', get_template_directory_uri() . '/js/fancybox/jquery.fancybox.css' );
