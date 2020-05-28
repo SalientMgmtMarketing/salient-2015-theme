696fe783ec46a7ee7863d8b78c3a47be9cf7eec8
Paul Stonier <pstonier@gmail.com>
2020-05-28 19:18:33 -0400

    changed style of underline for links to background image

--- content-none.php
+++ content-none.php
@@ -18,26 +18,26 @@
  */
 ?>
 
-<section class="no-results not-found">
-    <header class="page-header">
-        <h1 class="page-title"><?php _e( 'Nothing Found', 'salient-2015' ); ?></h1>
-    </header><!-- .page-header -->
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-header hero">
+        <div class="wrap">
+            <?php if ( function_exists('yoast_breadcrumb') && ! is_front_page() ) { yoast_breadcrumb('<p id="breadcrumbs">', '</p>'); } ?>
+            <h1 class="page-title"><?php _e( 'Nothing Found', 'salient-2015' ); ?></h1>
+        </div>
+    </header><!-- .entry-header -->
 
-    <div class="page-content">
-        <?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+    <div class="wrap">
 
+        <div class="entry-content">
+            <?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
             <p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'salient-2015' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
-
-        <?php elseif ( is_search() ) : ?>
-
-            <p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'salient-2015' ); ?></p>
-            <?php get_search_form(); ?>
-
-        <?php else : ?>
-
-            <p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'salient-2015' ); ?></p>
-            <?php get_search_form(); ?>
-
-        <?php endif; ?>
-    </div><!-- .page-content -->
-</section><!-- .no-results -->
+            <?php elseif ( is_search() ) : ?>
+                <p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'salient-2015' ); ?></p>
+                <?php get_search_form(); ?>
+            <?php else : ?>
+                <p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'salient-2015' ); ?></p>
+                <?php get_search_form(); ?>
+            <?php endif; ?>
+        </div><!-- .entry-content -->
+    </div><!-- .wrap -->
+</article><!-- .no-results -->
