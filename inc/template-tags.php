87c0e4d1e0c6b451c0cbc8163402635fbe163f7a
Paul Stonier <pstonier@gmail.com>
2020-09-28 09:44:20 -0400

    Added CoAuthor Support

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -12,26 +12,26 @@ if ( ! function_exists( 'salient_2015_paging_nav' ) ) :
  * Display navigation to next/previous set of posts when applicable.
  */
 function salient_2015_paging_nav() {
-    // Don't print empty markup if there's only one page.
-    if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
-        return;
-    }
-    ?>
-    <nav class="navigation paging-navigation" role="navigation">
-        <h2 class="screen-reader-text"><?php _e( 'Posts navigation', 'salient-2015' ); ?></h2>
-        <div class="nav-links">
+	// Don't print empty markup if there's only one page.
+	if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
+		return;
+	}
+	?>
+	<nav class="navigation paging-navigation" role="navigation">
+		<h2 class="screen-reader-text"><?php _e( 'Posts navigation', 'salient-2015' ); ?></h2>
+		<div class="nav-links">
 
-            <?php if ( get_next_posts_link() ) : ?>
-            <div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav" aria-hidden="true">&larr;</span> Older posts', 'salient-2015' ) ); ?></div>
-            <?php endif; ?>
+			<?php if ( get_next_posts_link() ) : ?>
+			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav" aria-hidden="true">&larr;</span> Older posts', 'salient-2015' ) ); ?></div>
+			<?php endif; ?>
 
-            <?php if ( get_previous_posts_link() ) : ?>
-            <div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav" aria-hidden="true">&rarr;</span>', 'salient-2015' ) ); ?></div>
-            <?php endif; ?>
+			<?php if ( get_previous_posts_link() ) : ?>
+			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav" aria-hidden="true">&rarr;</span>', 'salient-2015' ) ); ?></div>
+			<?php endif; ?>
 
-        </div><!-- .nav-links -->
-    </nav><!-- .navigation -->
-    <?php
+		</div><!-- .nav-links -->
+	</nav><!-- .navigation -->
+	<?php
 }
 endif;
 
@@ -40,91 +40,100 @@ if ( ! function_exists( 'salient_2015_post_nav' ) ) :
  * Display navigation to next/previous post when applicable.
  */
 function salient_2015_post_nav() {
-    // Don't print empty markup if there's nowhere to navigate.
-    $previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
-    $next     = get_adjacent_post( false, '', false );
+	// Don't print empty markup if there's nowhere to navigate.
+	$previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
+	$next     = get_adjacent_post( false, '', false );
 
-    if ( ! $next && ! $previous ) {
-        return;
-    }
-    ?>
-    <nav class="navigation post-navigation" role="navigation">
-        <h2 class="screen-reader-text"><?php _e( 'Post navigation', 'salient-2015' ); ?></h2>
-        <div class="nav-links">
-            <?php
-                previous_post_link( '<div class="nav-previous">%link</div>', _x( '<span class="meta-nav" aria-hidden="true">&larr;</span>&nbsp;%title', 'Previous post link', 'salient-2015' ) );
-                next_post_link(     '<div class="nav-next">%link</div>',     _x( '%title&nbsp;<span class="meta-nav" aria-hidden="true">&rarr;</span>', 'Next post link',     'salient-2015' ) );
-            ?>
-        </div><!-- .nav-links -->
-    </nav><!-- .navigation -->
-    <?php
+	if ( ! $next && ! $previous ) {
+		return;
+	}
+	?>
+	<nav class="navigation post-navigation" role="navigation">
+		<h2 class="screen-reader-text"><?php _e( 'Post navigation', 'salient-2015' ); ?></h2>
+		<div class="nav-links">
+			<?php
+				previous_post_link( '<div class="nav-previous">%link</div>', _x( '<span class="meta-nav" aria-hidden="true">&larr;</span>&nbsp;%title', 'Previous post link', 'salient-2015' ) );
+				next_post_link(     '<div class="nav-next">%link</div>',     _x( '%title&nbsp;<span class="meta-nav" aria-hidden="true">&rarr;</span>', 'Next post link',     'salient-2015' ) );
+			?>
+		</div><!-- .nav-links -->
+	</nav><!-- .navigation -->
+	<?php
 }
 endif;
 
 if ( ! function_exists( 'salient_2015_posted_on' ) ) :
-/**
- * Prints HTML with meta information for the current post-date/time and author.
- */
-function salient_2015_posted_on() {
-    $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-    if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-        $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-    }
+	/**
+	 * Prints HTML with meta information for the current post-date/time and author.
+	 */
+	function salient_2015_posted_on() {
+		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+				$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+		}
 
-    $time_string = sprintf( $time_string,
-        esc_attr( get_the_date( 'c' ) ),
-        esc_html( get_the_date() ),
-        esc_attr( get_the_modified_date( 'c' ) ),
-        esc_html( get_the_modified_date() )
-    );
+				$time_string = sprintf(
+					$time_string,
+					esc_attr( get_the_date( 'c' ) ),
+					esc_html( get_the_date() ),
+					esc_attr( get_the_modified_date( 'c' ) ),
+					esc_html( get_the_modified_date() )
+				);
 
-    $posted_on = sprintf(
-        _x( 'Posted on %s', 'post date', 'salient-2015' ),
-        '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-    );
+		$posted_on = sprintf(
+			_x( 'Posted on %s', 'post date', 'salient-2015' ),
+			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+		);
 
-    $byline = sprintf(
-        _x( 'by %s', 'post author', 'salient-2015' ),
-        '<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-    );
+		if ( function_exists( 'coauthors_posts_links' ) ) :
 
-    if ( get_field('alternative_author') ) {
-        echo '<span class="posted-on">' . $posted_on . '</span><span class="byline">by <span class="author vcard"> ' . get_field('alternative_author') . '</span></span>';
-    } else {
-        echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>';
-    }
+			$byline = sprintf(
+				_x( 'by %s', 'post author', 'salient-2015' ),
+				'<span class="author vcard">' . coauthors_posts_links( null, null, null, null, false ) . '</a></span>'
+			);
+		else :
+			$byline = sprintf(
+				_x( 'by %s', 'post author', 'salient-2015' ),
+				'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+			);
+		endif;
+
+		if ( get_field('alternative_author') ) {
+			echo '<span class="posted-on">' . $posted_on . '</span><span class="byline">by <span class="author vcard"> ' . get_field('alternative_author') . '</span></span>';
+		} else {
+			echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>';
+		}
 
 
-}
-endif;
+	}
+	endif;
 
 if ( ! function_exists( 'salient_2015_entry_footer' ) ) :
 /**
  * Prints HTML with meta information for the categories, tags and comments.
  */
 function salient_2015_entry_footer() {
-    // Hide category and tag text for pages.
-    if ( 'post' == get_post_type() ) {
-        /* translators: used between list items, there is a space after the comma */
-        $categories_list = get_the_category_list( __( ', ', 'salient-2015' ) );
-        if ( $categories_list && salient_2015_categorized_blog() ) {
-            printf( '<span class="cat-links">' . __( 'Posted in %1$s', 'salient-2015' ) . '</span>', $categories_list );
-        }
+	// Hide category and tag text for pages.
+	if ( 'post' == get_post_type() ) {
+		/* translators: used between list items, there is a space after the comma */
+		$categories_list = get_the_category_list( __( ', ', 'salient-2015' ) );
+		if ( $categories_list && salient_2015_categorized_blog() ) {
+			printf( '<span class="cat-links">' . __( 'Posted in %1$s', 'salient-2015' ) . '</span>', $categories_list );
+		}
 
-        /* translators: used between list items, there is a space after the comma */
-        $tags_list = get_the_tag_list( '', __( ', ', 'salient-2015' ) );
-        if ( $tags_list ) {
-            printf( '<span class="tags-links">' . __( 'Tagged %1$s', 'salient-2015' ) . '</span>', $tags_list );
-        }
-    }
+		/* translators: used between list items, there is a space after the comma */
+		$tags_list = get_the_tag_list( '', __( ', ', 'salient-2015' ) );
+		if ( $tags_list ) {
+			printf( '<span class="tags-links">' . __( 'Tagged %1$s', 'salient-2015' ) . '</span>', $tags_list );
+		}
+	}
 
-    if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-        echo '<span class="comments-link">';
-        comments_popup_link( __( 'Leave a comment', 'salient-2015' ), __( '1 Comment', 'salient-2015' ), __( '% Comments', 'salient-2015' ) );
-        echo '</span>';
-    }
+	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+		echo '<span class="comments-link">';
+		comments_popup_link( __( 'Leave a comment', 'salient-2015' ), __( '1 Comment', 'salient-2015' ), __( '% Comments', 'salient-2015' ) );
+		echo '</span>';
+	}
 
-    edit_post_link( __( 'Edit', 'salient-2015' ), '<span class="edit-link">', '</span>' );
+	edit_post_link( __( 'Edit', 'salient-2015' ), '<span class="edit-link">', '</span>' );
 }
 endif;
 
@@ -140,56 +149,56 @@ if ( ! function_exists( 'the_archive_title' ) ) :
  * @param string $after  Optional. Content to append to the title. Default empty.
  */
 function the_archive_title( $before = '', $after = '' ) {
-    if ( is_category() ) {
-        $title = sprintf( __( 'Category: %s', 'salient-2015' ), single_cat_title( '', false ) );
-    } elseif ( is_tag() ) {
-        $title = sprintf( __( 'Tag: %s', 'salient-2015' ), single_tag_title( '', false ) );
-    } elseif ( is_author() ) {
-        $title = sprintf( __( 'Author: %s', 'salient-2015' ), '<span class="vcard">' . get_the_author() . '</span>' );
-    } elseif ( is_year() ) {
-        $title = sprintf( __( 'Year: %s', 'salient-2015' ), get_the_date( _x( 'Y', 'yearly archives date format', 'salient-2015' ) ) );
-    } elseif ( is_month() ) {
-        $title = sprintf( __( 'Month: %s', 'salient-2015' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'salient-2015' ) ) );
-    } elseif ( is_day() ) {
-        $title = sprintf( __( 'Day: %s', 'salient-2015' ), get_the_date( _x( 'F j, Y', 'daily archives date format', 'salient-2015' ) ) );
-    } elseif ( is_tax( 'post_format', 'post-format-aside' ) ) {
-        $title = _x( 'Asides', 'post format archive title', 'salient-2015' );
-    } elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) {
-        $title = _x( 'Galleries', 'post format archive title', 'salient-2015' );
-    } elseif ( is_tax( 'post_format', 'post-format-image' ) ) {
-        $title = _x( 'Images', 'post format archive title', 'salient-2015' );
-    } elseif ( is_tax( 'post_format', 'post-format-video' ) ) {
-        $title = _x( 'Videos', 'post format archive title', 'salient-2015' );
-    } elseif ( is_tax( 'post_format', 'post-format-quote' ) ) {
-        $title = _x( 'Quotes', 'post format archive title', 'salient-2015' );
-    } elseif ( is_tax( 'post_format', 'post-format-link' ) ) {
-        $title = _x( 'Links', 'post format archive title', 'salient-2015' );
-    } elseif ( is_tax( 'post_format', 'post-format-status' ) ) {
-        $title = _x( 'Statuses', 'post format archive title', 'salient-2015' );
-    } elseif ( is_tax( 'post_format', 'post-format-audio' ) ) {
-        $title = _x( 'Audio', 'post format archive title', 'salient-2015' );
-    } elseif ( is_tax( 'post_format', 'post-format-chat' ) ) {
-        $title = _x( 'Chats', 'post format archive title', 'salient-2015' );
-    } elseif ( is_post_type_archive() ) {
-        $title = sprintf( __( 'Archives: %s', 'salient-2015' ), post_type_archive_title( '', false ) );
-    } elseif ( is_tax() ) {
-        $tax = get_taxonomy( get_queried_object()->taxonomy );
-        /* translators: 1: Taxonomy singular name, 2: Current taxonomy term */
-        $title = sprintf( __( '%1$s: %2$s', 'salient-2015' ), $tax->labels->singular_name, single_term_title( '', false ) );
-    } else {
-        $title = __( 'Archives', 'salient-2015' );
-    }
+	if ( is_category() ) {
+		$title = sprintf( __( 'Category: %s', 'salient-2015' ), single_cat_title( '', false ) );
+	} elseif ( is_tag() ) {
+		$title = sprintf( __( 'Tag: %s', 'salient-2015' ), single_tag_title( '', false ) );
+	} elseif ( is_author() ) {
+		$title = sprintf( __( 'Author: %s', 'salient-2015' ), '<span class="vcard">' . get_the_author() . '</span>' );
+	} elseif ( is_year() ) {
+		$title = sprintf( __( 'Year: %s', 'salient-2015' ), get_the_date( _x( 'Y', 'yearly archives date format', 'salient-2015' ) ) );
+	} elseif ( is_month() ) {
+		$title = sprintf( __( 'Month: %s', 'salient-2015' ), get_the_date( _x( 'F Y', 'monthly archives date format', 'salient-2015' ) ) );
+	} elseif ( is_day() ) {
+		$title = sprintf( __( 'Day: %s', 'salient-2015' ), get_the_date( _x( 'F j, Y', 'daily archives date format', 'salient-2015' ) ) );
+	} elseif ( is_tax( 'post_format', 'post-format-aside' ) ) {
+		$title = _x( 'Asides', 'post format archive title', 'salient-2015' );
+	} elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) {
+		$title = _x( 'Galleries', 'post format archive title', 'salient-2015' );
+	} elseif ( is_tax( 'post_format', 'post-format-image' ) ) {
+		$title = _x( 'Images', 'post format archive title', 'salient-2015' );
+	} elseif ( is_tax( 'post_format', 'post-format-video' ) ) {
+		$title = _x( 'Videos', 'post format archive title', 'salient-2015' );
+	} elseif ( is_tax( 'post_format', 'post-format-quote' ) ) {
+		$title = _x( 'Quotes', 'post format archive title', 'salient-2015' );
+	} elseif ( is_tax( 'post_format', 'post-format-link' ) ) {
+		$title = _x( 'Links', 'post format archive title', 'salient-2015' );
+	} elseif ( is_tax( 'post_format', 'post-format-status' ) ) {
+		$title = _x( 'Statuses', 'post format archive title', 'salient-2015' );
+	} elseif ( is_tax( 'post_format', 'post-format-audio' ) ) {
+		$title = _x( 'Audio', 'post format archive title', 'salient-2015' );
+	} elseif ( is_tax( 'post_format', 'post-format-chat' ) ) {
+		$title = _x( 'Chats', 'post format archive title', 'salient-2015' );
+	} elseif ( is_post_type_archive() ) {
+		$title = sprintf( __( 'Archives: %s', 'salient-2015' ), post_type_archive_title( '', false ) );
+	} elseif ( is_tax() ) {
+		$tax = get_taxonomy( get_queried_object()->taxonomy );
+		/* translators: 1: Taxonomy singular name, 2: Current taxonomy term */
+		$title = sprintf( __( '%1$s: %2$s', 'salient-2015' ), $tax->labels->singular_name, single_term_title( '', false ) );
+	} else {
+		$title = __( 'Archives', 'salient-2015' );
+	}
 
-    /**
-     * Filter the archive title.
-     *
-     * @param string $title Archive title to be displayed.
-     */
-    $title = apply_filters( 'get_the_archive_title', $title );
+	/**
+	 * Filter the archive title.
+	 *
+	 * @param string $title Archive title to be displayed.
+	 */
+	$title = apply_filters( 'get_the_archive_title', $title );
 
-    if ( ! empty( $title ) ) {
-        echo $before . $title . $after;
-    }
+	if ( ! empty( $title ) ) {
+		echo $before . $title . $after;
+	}
 }
 endif;
 
@@ -205,18 +214,18 @@ if ( ! function_exists( 'the_archive_description' ) ) :
  * @param string $after  Optional. Content to append to the description. Default empty.
  */
 function the_archive_description( $before = '', $after = '' ) {
-    $description = apply_filters( 'get_the_archive_description', term_description() );
+	$description = apply_filters( 'get_the_archive_description', term_description() );
 
-    if ( ! empty( $description ) ) {
-        /**
-         * Filter the archive description.
-         *
-         * @see term_description()
-         *
-         * @param string $description Archive description to be displayed.
-         */
-        echo $before . $description . $after;
-    }
+	if ( ! empty( $description ) ) {
+		/**
+		 * Filter the archive description.
+		 *
+		 * @see term_description()
+		 *
+		 * @param string $description Archive description to be displayed.
+		 */
+		echo $before . $description . $after;
+	}
 }
 endif;
 
@@ -226,37 +235,37 @@ endif;
  * @return bool
  */
 function salient_2015_categorized_blog() {
-    if ( false === ( $all_the_cool_cats = get_transient( 'salient_2015_categories' ) ) ) {
-        // Create an array of all the categories that are attached to posts.
-        $all_the_cool_cats = get_categories( array(
-            'fields'     => 'ids',
-            'hide_empty' => 1,
+	if ( false === ( $all_the_cool_cats = get_transient( 'salient_2015_categories' ) ) ) {
+		// Create an array of all the categories that are attached to posts.
+		$all_the_cool_cats = get_categories( array(
+			'fields'     => 'ids',
+			'hide_empty' => 1,
 
-            // We only need to know if there is more than one category.
-            'number'     => 2,
-        ) );
+			// We only need to know if there is more than one category.
+			'number'     => 2,
+		) );
 
-        // Count the number of categories that are attached to the posts.
-        $all_the_cool_cats = count( $all_the_cool_cats );
+		// Count the number of categories that are attached to the posts.
+		$all_the_cool_cats = count( $all_the_cool_cats );
 
-        set_transient( 'salient_2015_categories', $all_the_cool_cats );
-    }
+		set_transient( 'salient_2015_categories', $all_the_cool_cats );
+	}
 
-    if ( $all_the_cool_cats > 1 ) {
-        // This blog has more than 1 category so salient_2015_categorized_blog should return true.
-        return true;
-    } else {
-        // This blog has only 1 category so salient_2015_categorized_blog should return false.
-        return false;
-    }
+	if ( $all_the_cool_cats > 1 ) {
+		// This blog has more than 1 category so salient_2015_categorized_blog should return true.
+		return true;
+	} else {
+		// This blog has only 1 category so salient_2015_categorized_blog should return false.
+		return false;
+	}
 }
 
 /**
  * Flush out the transients used in salient_2015_categorized_blog.
  */
 function salient_2015_category_transient_flusher() {
-    // Like, beat it. Dig?
-    delete_transient( 'salient_2015_categories' );
+	// Like, beat it. Dig?
+	delete_transient( 'salient_2015_categories' );
 }
 add_action( 'edit_category', 'salient_2015_category_transient_flusher' );
 add_action( 'save_post',     'salient_2015_category_transient_flusher' );
